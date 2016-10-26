df6b9cc11816350aab13023dff29a95699afef1a
chris walters <chri5walters@msn.com>
2016-10-26 22:33:12 +0100

    Started Implementing the Waypoint Objects and Events to trigger on exit etc. Waiting for Changes on Splitscreen branch for RaceManager / Player Objects

---
+++ Assets/Scripts/Controllers/WayPointController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+using Objects;
+
+namespace Controllers
+{
+    public delegate void PlayerTriggerExit(PlayerTriggerExitEventArgs EventArgs);
+
+    public class WayPointController : MonoBehaviour
+    {
+        public event PlayerTriggerExit OnPlayerTriggerExit;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnTriggerExit(Collider p_OtherCollider)
+        {
+            if (p_OtherCollider.name == "PlayerTriggerCollider")
+            {
+                GameObject l_OtherGameObject = p_OtherCollider.gameObject;
+
+                /*
+                 * Need a method to access the Player object for the Player from the PlayerTriggerCollider??                 * 
+                 * 
+                 * Store a Reference in the Kart Collider and then you just have to work your way up to there? 
+                 * (Could always move the Collider up to the Top Level?)
+                 *  OR
+                 * Go to the Race Manager and Find the Player by the GameObject?
+                 * 
+                */
+
+                if (OnPlayerTriggerExit != null)
+                    OnPlayerTriggerExit(new PlayerTriggerExitEventArgs(this, /*l_Player,*/ DateTime.Now));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/PlayerTriggerExitEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Controllers;
+
+namespace Objects
+{
+    public class PlayerTriggerExitEventArgs : EventArgs
+    {
+        public WayPointController e_Waypoint;
+        //public Player e_Player;
+        public DateTime e_Timestamp;
+
+        public PlayerTriggerExitEventArgs(WayPointController p_Waypoint, /*Player p_Player,*/ DateTime p_Timestamp) 
+        {
+            e_Waypoint = p_Waypoint;
+            //e_Player = p_Player;
+            e_Timestamp = p_Timestamp;
+        }
+    }
+}
